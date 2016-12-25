300cf5b98d75211b5833f92c825ee1894182d65a
Alexander Tsvetkov <iammamba@gmail.com>
2016-12-25 22:48:32 +0100

    added final version of demo code

---
+++ src/CSharp7NewFeaturesCore/CSharp7NewFeaturesCore/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var number in new object[] { 123, "2017", "-1", "1a2b3c", '5' })
+        {
+            try
+            {
+                (bool parsed, int result) = IntegerParser.TryParse(number);
+                if (parsed)
+                {
+                    Console.WriteLine($"Parsed {number.GetType()} {number}, got {result}");
+                }
+                else
+                {
+                    Console.WriteLine($"Could not parse {number.GetType()} {number}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error when parsing: {e.Message}");
+            }
+        }
+    }
+}
+
+public static class IntegerParser
+{
+    private const int MinDigit = '0';
+    private const int MaxDigit = '9';
+
+    public static (bool success, int result) TryParse(object number)
+    {
+        switch (number)
+        {
+            case int i:
+                return (true, i);
+            case string s:
+                (bool isParsed, int value) ParseString(string str)
+                {
+                    var res = 0;
+                    for (var i = 0; i < str.Length; i++)
+                    {
+                        var charCode = (int)str[i];
+                        if (charCode < MinDigit || charCode > MaxDigit)
+                        {
+                            return (false, res);
+                        }
+
+                        res = res * 10 + (charCode - MinDigit);
+                    }
+
+                    return (true, res);
+                }
+
+                return ParseString(s);
+            default:
+                throw new NotSupportedException($"{nameof(IntegerParser)} only accepts strings ans integers, type '{number.GetType()}' is not supported");
+        }
+    }
+}
